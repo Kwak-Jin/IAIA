0eb38ffcb0a077094900cacb51d652154d0482ae
Jorry BBong <147172379+Kwak-Jin@users.noreply.github.com>
2024-12-03 13:55:35 +0900

    Renew

--- Project/Robot Automation/src/demo_move.py
+++ Project/Robot Automation/src/demo_move.py
@@ -15,17 +15,24 @@ def main():
         
         ur5e = MoveGroupPythonInterface(real="real", gripper="Vacuum")
         init_pose_joints = [tau/4, -tau/4, tau/4, -tau/4, -tau/4, 0.0]          # tau = 2 * pi
-        target_pose_abs_xyz = [-0.003, -0.05, -0.305]
+        init_pose_joints = [pi/2, -pi*2/3, pi/3, -pi/4, -pi/2, 0.0]          # tau = 2 * pi
+        waypoint_init_pose_joints = [tau/4, -tau/4, tau/4, -tau/4, -tau/4, 0.0]
+        target_pose_abs_xyz = [0.0900-7*grid_jump, 0.0585+7*grid_jump, -0.305]
         target_pose_abs_rpy = [0.00, 0.00, 0.00]
         print("============ Moving initial pose using a joint state goal ...")
         # ur5e.move_to_standby()
         ur5e.go_to_joint_abs(init_pose_joints)
-        grip_pose_joints = [0, -pi/2, pi/2, -pi/2, -pi/2, 0.0]
+        ur5e.go_to_joint_abs(waypoint_init_pose_joints)
+        waypoint_grip_pose_joints = [pi/3, -pi/3, pi/2,-2*pi/3, -pi/2, 0.0]
+        # grip_pose_joints = [0, -pi/2, pi/2, -pi/2, -pi/2, 0.0]
+        grip_pose_joints = [50*pi/180, -pi/3, 100*pi/180, -130*pi/180, -95*pi/180, 0.0]
         input("============ Press `Enter` to execute a movement using a absolute pose 3...")
         cnt = 1
-        while 1:            
+        while 1:
+            ur5e.go_to_joint_abs(waypoint_grip_pose_joints)
             ur5e.go_to_joint_abs(grip_pose_joints)
             ur5e.grip_on()
+            ur5e.go_to_joint_abs(waypoint_init_pose_joints)
             ur5e.go_to_joint_abs(init_pose_joints)
             print(f"Turn = {cnt} AI's turn")
             x,y = input("============ Press 2 Integer values for Gomoku...").split()
@@ -35,9 +42,11 @@ def main():
             # if (x)>7 or x<-7 (y)>7 or y<-7:
             #     print("Wrong position")
             #     continue
+            ur5e.go_to_joint_abs(waypoint_init_pose_joints)
             target_pose_abs_xyz_go = [target_pose_abs_xyz[0]+float(x)*grid_jump, target_pose_abs_xyz[1]+float(y)*grid_jump,-0.305]
             ur5e.go_to_pose_rel(target_pose_abs_xyz_go, target_pose_abs_rpy)
             ur5e.grip_off()
+
             cnt = cnt + 1
 
 
